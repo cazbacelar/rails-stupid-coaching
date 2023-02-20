4cb9decfe5bfe1daec0b2f3efc734867fb9d663b
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-02-20 12:37:14 +0000

    Stupid Coaching - /ask and /answer pages completed

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def ask
+    @answer = params[:question]
+  end
+
+  def answer
+    @answer = params[:question]
+    if params[:question] == 'I am going to work'
+      p 'Great!'
+    elsif params[:question].ends_with?('?')
+      p 'Silly question, get dressed and go to work!'
+    else
+      p 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
