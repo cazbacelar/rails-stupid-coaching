843126696ef6a3a9a4de2a52104d8ea42a1e0519
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-02-20 14:15:35 +0000

    Stupid Coaching style added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,12 @@ class PagesController < ApplicationController
 
   def answer
     @answer = params[:question]
-    if params[:question] == 'I am going to work'
-      p 'Great!'
-    elsif params[:question].ends_with?('?')
-      p 'Silly question, get dressed and go to work!'
-    else
-      p 'I don\'t care, get dressed and go to work!'
-    end
+    # if params[:question] == 'I am going to work'
+    #   p 'Great!'
+    # elsif params[:question].ends_with?('?')
+    #   p 'Silly question, get dressed and go to work!'
+    # else
+    #   p 'I don\'t care, get dressed and go to work!'
+    # end
   end
 end
